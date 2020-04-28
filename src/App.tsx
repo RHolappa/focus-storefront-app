919c481948cccf157c8b9625e95c435b6c8e8843
Risto Holappa <risto_holappa@hotmail.com>
2020-04-28 11:35:45 +0300

    add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import './App.css';
+import * as React from "react";
+//import * as _ from 'lodash';
 
-function App() {
-  const now = new Date();
-  const value = 200;
-  const string = 'OK';
+interface AppState {}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-      <p> Hello! It is now {now.toString()}</p>
-      <p>
-        {value} - {string}
-      </p>
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
 
-      </header>
-    </div>
-  );
+    this.state = {
+      //projects: contents,
+    };
+  }
+
+  public render() {
+    return (
+      <div className="app container-fluid">
+        <header className="row app-header">
+          <div className="col">
+            <h1 className="app-title">Storefront</h1>
+          </div>
+        </header>
+        <div className="app-container"></div>
+        <div className="row justify-content-center">
+          <div className="col col-lg-10 col-xl-8"></div>
+        </div>
+        <div className="row justify-content-center">
+          <footer className="app-footer col"></footer>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
